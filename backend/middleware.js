eb92539d16219fcdcee157e2cbf81cfe45e5dc1d
het4399 <het.sojitra4399@gmail.com>
2025-03-15 14:45:07 +0530

    Added Middleware

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,2 +1,17 @@
-const jwt_secret = require("./config")
+const jwt_secret = require("./config");
 
+const jwt = require("jsonwebtoken");
+function autmiddlware(req, res, next) {
+  const headers = req.headers.authorization;
+  const token = headers.split(" ")[1];
+  const a = jwt.verify(token, jwt_secret);
+  if (a.userId) {
+    next();
+  }
+  else{
+    res.status(403).json({
+        message:"Invalid token"
+    })
+  }
+}
+module.exports = autmiddlware;
